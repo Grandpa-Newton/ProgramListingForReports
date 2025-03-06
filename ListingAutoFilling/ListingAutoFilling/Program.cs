fd51a27103d1805fe15ec25a2729a82b3033b5f9
Gromyko <ivan.gromyko.2804@gmail.com>
2025-03-06 17:40:51 +0300

    Adding test text in existing document

--- ListingAutoFilling/ListingAutoFilling/Program.cs
+++ ListingAutoFilling/ListingAutoFilling/Program.cs
@@ -5,9 +5,25 @@ namespace ListingAutoFilling
 {
     public class Program
     {
+        private const string DocFileName = @"C:\university\tst.docx";
         public static void Main(string[] args)
         {
+            using WordprocessingDocument wordDoc = WordprocessingDocument.Open(DocFileName, true);
+            MainDocumentPart mainPart = wordDoc.MainDocumentPart ?? wordDoc.AddMainDocumentPart();
             
+            Body body = mainPart.Document.AppendChild(new Body());
+
+            Run run = new Run();
+            RunProperties runProps = new RunProperties();
+            Bold bold = new Bold();
+            runProps.Append(bold);
+            run.Append(runProps);
+            run.Append(new Text("Привет"));
+
+            Paragraph para = new Paragraph();
+            para.Append(run);
+
+            body.Append(para);
         }
     }
 }
