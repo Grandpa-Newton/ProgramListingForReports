841ab931358af31fc9cd49f3524a3f29e20e5827
Gromyko <ivan.gromyko.2804@gmail.com>
2025-03-06 17:26:46 +0300

    Creating project and adding OpenXML package

---
+++ ListingAutoFilling/ListingAutoFilling/Program.cs
@@ -0,0 +1,13 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace ListingAutoFilling
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
